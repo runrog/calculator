726674fabf00cf4ae1c237b32df6046e7e1be26a
bighappyface <anomalous20@gmail.com>
2020-04-09 14:41:24 -0500

    refactor(tests): add CurrencySwitcher.spec

---
+++ src/tests/unit/CurrencySwitcher.spec.js
@@ -0,0 +1,62 @@
+import { mount } from "@/tests/jest.setup";
+import Vue from "vue";
+import CurrencySwitcher from "@/components/CurrencySwitcher.vue";
+
+const mockComponent = Vue.component("mockComponent", {
+  components: {
+    CurrencySwitcher
+  },
+  data() {
+    return {
+      number: 1.23,
+      currency: "USD"
+    };
+  },
+  template:
+    '<div>{{ $t("rax_label") }}<i18n-n :value="number" format="currency"></i18n-n><CurrencySwitcher v-model="currency" /></div>'
+});
+
+describe("CurrencySwitcher", () => {
+  it("renders a total with USD currency", async () => {
+    const wrapper = mount(mockComponent);
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toContain("Rackspace Email");
+    expect(wrapper.text()).toContain("$1.23");
+  });
+
+  it("renders a total with AUD currency", async () => {
+    const wrapper = mount(mockComponent);
+    wrapper.setData({
+      currency: "AUD",
+      number: 2.34
+    });
+    await wrapper.vm.$nextTick();
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toContain("Rackspace Email");
+    expect(wrapper.text()).toContain("$2.34");
+  });
+
+  it("renders a total with GBP currency", async () => {
+    const wrapper = mount(mockComponent);
+    wrapper.setData({
+      currency: "GBP",
+      number: 3.45
+    });
+    await wrapper.vm.$nextTick();
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toContain("Rackspace Email");
+    expect(wrapper.text()).toContain("£3.45");
+  });
+
+  it("renders a total with EUR currency", async () => {
+    const wrapper = mount(mockComponent);
+    wrapper.setData({
+      currency: "EUR",
+      number: 4.56
+    });
+    await wrapper.vm.$nextTick();
+    await wrapper.vm.$nextTick();
+    expect(wrapper.text()).toContain("Rackspace Email");
+    expect(wrapper.text()).toContain("€4.56");
+  });
+});
--- src/tests/unit/email/EmailCalculator.spec.js
+++ src/tests/unit/email/EmailCalculator.spec.js
@@ -1,4 +1,3 @@
-import { mount } from "@/tests/jest.setup";
 import EmailCalculator from "@/components/email/EmailCalculator.vue";
 import prices from "@/prices/cloud_office.json";
 
@@ -7,39 +6,6 @@ describe("EmailCalculator", () => {
     it("rax_total calculates correct total", () => {
       const localThis = { prices: prices, rax_qty: "2", currency: "USD" };
       expect(EmailCalculator.computed.rax_total.call(localThis)).toBe(5.98);
-    });
-
-    it("renders a total with USD currency", async () => {
-      const wrapper = mount(EmailCalculator);
-      wrapper.setData({
-        currency: "USD",
-        prices: prices,
-        rax_qty: 2,
-        raxplus: false,
-        hex_qty: 0,
-        office_2016: 0,
-        email_archiving: false
-      });
-
-      await wrapper.vm.$nextTick();
-      expect(wrapper.text()).toContain("$5.98");
-    });
-
-    it("renders a total with GBP currency", async () => {
-      const wrapper = mount(EmailCalculator);
-      wrapper.setData({
-        currency: "GBP",
-        prices: prices,
-        rax_qty: 2,
-        raxplus: false,
-        hex_qty: 0,
-        office_2016: 0,
-        email_archiving: false
-      });
-
-      await wrapper.vm.$nextTick();
-      await wrapper.vm.$nextTick();
-      expect(wrapper.text()).toContain("£4.78");
     });
   });
 });
