88f9cce9e48a4838a3ab659143befc7276985a5b
bighappyface <anomalous20@gmail.com>
2020-04-10 14:05:16 -0500

    feat(pdb_vue): add pdb_vue support and emailcalculator block

---
+++ blocks/emailcalculator/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="emailcalculator">
+    <EmailCalculator />
+  </div>
+</template>
+
+<script>
+import EmailCalculator from "@/components/email/EmailCalculator.vue";
+
+export default {
+  name: "emailcalculator",
+  components: {
+    EmailCalculator
+  },
+  data() {
+    return {
+      instanceId: ""
+    };
+  },
+  beforeMount() {
+    // Get the ID tag containing the drupal instance id.
+    this.instanceId = this.$parent.$el.id;
+  }
+};
+</script>
+
+<style scoped>
+.emailcalculator {
+  background-color: aqua;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const { singleInstances } = require("vue-cli-plugin-drupal-pdb");
+
+// Set this to the path in Drupal where your pdb_vue 'dist' directory lives.
+const drupalPath = "/modules/rackspace/pdb_vue/dist/";
+
 module.exports = {
   pluginOptions: {
     i18n: {
@@ -6,5 +11,11 @@ module.exports = {
       localeDir: "locales",
       enableInSFC: false
     }
-  }
+  },
+  // Disable filename hashing in Drupal.
+  filenameHashing: false,
+  // Generate individual files for each instance.
+  pages: singleInstances(),
+  // Set the public path so images will work.
+  publicPath: process.env.NODE_ENV === "production" ? drupalPath : "/"
 };
